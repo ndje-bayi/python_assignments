dd44e7ebafa5506e703d9b4f3204a7b68443c939
ndje bayi <ndjebayi.boris@ictuniversity.edu.cm>
2022-01-31 22:25:37 +0100

    updates

--- secondes.py
+++ secondes.py
@@ -1,6 +1,9 @@
+"""
+This program prompts the user to enter a number of seconds; then it breaks it down into years, months, weeks, minutes
+and seconds then finaly prints the result
 
-seconds = input("Enter the number of seconds: >>> ")
-
+"""
+#The scale of seconds
 SECONDS_IN_YEAR = 31536000
 SECONDS_IN_MONTH = 2592000
 SECONDS_IN_WEEK = 604800
@@ -8,7 +11,21 @@ SECONDS_IN_DAY = 86400
 SECONDS_IN_HOUR = 3600
 SECONDS_IN_MINUTE = 60
 
-if seconds.isdecimal():
+
+def main():
+    seconds = input("Enter the number of seconds: >>> ")
+    if seconds.isdecimal():
+        result = break_down(seconds)
+        print("""The number of years is: {}\nThe number of months is: {}\nThe number of weeks is: {}\
+            \nThe number of days is: {}\nThe number of hours is: {}\nThe number of minutes is: {}\
+            \nThe number of seconds is: {}""".format(result[0],
+            result[1], result[2], result[3], result[4], result[5], result[6]))
+        
+    else:
+        print(seconds, "is an invalid input")
+        
+
+def break_down(seconds):
     seconds = int(seconds)
     years = int(seconds / SECONDS_IN_YEAR)
     remaining = seconds % SECONDS_IN_YEAR
@@ -22,9 +39,7 @@ if seconds.isdecimal():
     remaining %= SECONDS_IN_HOUR
     minutes = int(remaining / SECONDS_IN_MINUTE)
     seconds = remaining % SECONDS_IN_MINUTE
-    print(f"The number of years is: {years}\nThe number of months is: {months}\nThe number of weeks is: {weeks}")
-    print(f"The number of days is: {days}\nThe number of hours is: {hours}\nThe number of minutes is: {minutes}")
-    print(f"And the number of seconds is: {seconds}")
+    return years, months, weeks, days, hours, minutes, seconds
 
-else:
-    print("The input is not valid")
+if __name__ == "__main__":
+    main()
